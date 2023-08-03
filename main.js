92ab4e7dc86f74d4d85a599007e908b1b0963464
JuanManuelGasinghini <113570244+JuanManuelGasinghini@users.noreply.github.com>
2023-08-02 23:35:36 -0300

    agregado css y solucionado error de consola

--- main.js
+++ main.js
@@ -3,26 +3,26 @@ document.addEventListener("DOMContentLoaded", function() {
     let signIn = document.getElementById("signIn");
     let nameInput = document.getElementById("nameInput");
     let title = document.getElementById("title");
-    
+
     signIn.onclick = function() {
         nameInput.style.maxHeight = "0";
         title.innerHTML = "Login";
         signUp.classList.add("disable");
         signIn.classList.remove("disable");
     }
-    
+
     signUp.onclick = function() {
         nameInput.style.maxHeight = "60px";
         title.innerHTML = "Registro";
         signUp.classList.remove("disable");
         signIn.classList.add("disable");
 
-        let name = nameInput.value.trim(); // Mover esta línea aquí
+        let name = nameInput.value.trim(); 
         if (name !== "") {
             localStorage.setItem("user_name", name);
         }
     }
-    
+
     let storedName = localStorage.getItem("user_name");
     if (storedName) {
         nameInput.value = storedName;
