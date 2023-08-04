b7eb71cc06f960f9a7a68ae166125e21f823fd7d
JuanManuelGasinghini <113570244+JuanManuelGasinghini@users.noreply.github.com>
2023-08-04 00:05:37 -0300

    detalles

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let signUp = document.getElementById("signUp");
-    let signIn = document.getElementById("signIn");
-    let nameInput = document.getElementById("nameInput");
-    let title = document.getElementById("title");
+    const signUp = document.getElementById("signUp");
+    const signIn = document.getElementById("signIn");
+    const nameInput = document.getElementById("nameInput");
+    const title = document.getElementById("title");
 
-    signIn.onclick = function() {
+    signIn.addEventListener("click", function() {
         nameInput.style.maxHeight = "0";
         title.innerHTML = "Login";
         signUp.classList.add("disable");
         signIn.classList.remove("disable");
-    }
+    });
 
-    signUp.onclick = function() {
+    signUp.addEventListener("click", function() {
         nameInput.style.maxHeight = "60px";
         title.innerHTML = "Registro";
         signUp.classList.remove("disable");
         signIn.classList.add("disable");
 
-        let name = nameInput.value.trim(); 
+        const name = nameInput.value.trim(); 
         if (name !== "") {
             localStorage.setItem("user_name", name);
         }
-    }
+    });
 
-    let storedName = localStorage.getItem("user_name");
+    const storedName = localStorage.getItem("user_name");
     if (storedName) {
         nameInput.value = storedName;
     }
